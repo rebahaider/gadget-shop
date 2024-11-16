ef1d37d8c849d8be96d08d9664b8bcfb1f61edde
rebahaider <rebahaiderema@gmail.com>
2024-11-16 20:41:10 +0600

    added react icons on the overview page

--- src/components/dashboard/Sidebar.jsx
+++ src/components/dashboard/Sidebar.jsx
@@ -1,18 +1,32 @@
 import { NavLink } from "react-router-dom"
+import { GrOverview } from "react-icons/gr";
+import { AiOutlineHome } from "react-icons/ai";
+import { RiLogoutCircleRLine } from "react-icons/ri";
+
+
 
 const Sidebar = () => {
     return (
         <div className="bg-yellow-100 border-r-2 border-black min-h-screen px-8 py-12">
             <h1 className="text-2xl font-bold mb-8">Gadget Shop</h1>
             <ul className="flex flex-col gap-4">
-                <li>
-                    <NavLink to="/dashboard/overview">Overview</NavLink>
+                <li className="p-2 border border-black rounded-md">
+                    <NavLink to="/dashboard/overview" className="flex items-center gap-2">
+                        <GrOverview />
+                        <p>Overview</p>
+                    </NavLink>
                 </li>
-                <li>
-                    <NavLink to="/">Home</NavLink>
+                <li className="p-2 border border-black rounded-md">
+                    <NavLink to="/dashboard/overview" className="flex items-center gap-2">
+                        <AiOutlineHome />
+                        <p>Home</p>
+                    </NavLink>
                 </li>
-                <li>
-                    <button className="btn btn-warning btn-outline">Logout</button>
+                <li className="p-2 border border-black rounded-md">
+                    <NavLink to="/dashboard/overview" className="flex items-center gap-2">
+                        <RiLogoutCircleRLine />
+                        <p>Log Out</p>
+                    </NavLink>
                 </li>
             </ul>
         </div>
